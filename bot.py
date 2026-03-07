8c3387c078c4a8e3c5745d0f47b0be18e65cd0db
yagubiskenderov483-code <yagubiskenderov483@gmail.com>
2026-03-08 00:13:52 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ from datetime import datetime
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 SUPPORT = "GiftExchangersSupport"
-MANAGER = "GiftExchangersManager"
-BOT_USERNAME = "GiftExchagersBot"
+MANAGER = "GiftExchangersManager
+BOT_USERNAME = "GiftExchangersBot"
 
 # ===== ДАННЫЕ =====
 deals = {}
